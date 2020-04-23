b6accede73ce286b585d58b84f2dfc0d70145f88
hisbvdis <hisbvdis@gmail.com>
2020-04-23 16:47:12 +0300

    Spaces recognizing added

--- table-schedule-converter/main.js
+++ table-schedule-converter/main.js
@@ -1,17 +1,19 @@
 const input = document.querySelector(".input");
-const div = document.querySelector("div");
 const output = document.querySelector(".output");
 
 input.oninput = function() {
   let arr = input.value.split("\n");
   let newArr = [];
   let resultStr = "";
+
   for (let item of arr) {
     let [hours, minScope] = item.split("\t");
-    let minScopeLength = minScope.length;
-    console.log(hours);
-    console.log(minScope);
-    for (let i = 1; i <= minScopeLength / 2; i++) {
+
+    if (minScope.includes(" ") === true) {
+      minScope = minScope.split(" ").join("");
+    }
+    
+    for (let i = 1; i <= minScope.length / 2; ) {
       newArr.push(hours + ":" + minScope.slice(0, 2));
       minScope = minScope.slice(2);
     }
