d0e0c9bb47cb5b30cb4b7a53955acaf5fffb78d7
hisbvdis <hisbvdis@gmail.com>
2020-04-28 17:48:26 +0300

    Modal with example image added

--- table-schedule-converter/main.js
+++ table-schedule-converter/main.js
@@ -1,6 +1,11 @@
 const input = document.querySelector(".input");
 const output = document.querySelector(".output");
+const exampleLink = document.querySelector(".exampleLink");
+const scheduleExample = document.querySelector(".scheduleExample");
+const modalUnderlay = document.querySelector(".modalUnderlay");
 
+
+// CONVERTING
 input.oninput = function() {
   let arr = input.value.split("\n");
   let newArr = [];
@@ -28,4 +33,41 @@ input.oninput = function() {
   output.value = resultStr;
   output.select();
   // document.execCommand("copy");
+}
+
+
+// MODALS
+exampleLink.onclick = function(e) {
+  e.preventDefault();
+  showModal(scheduleExample);
+  
+  if (!document.querySelector(".scheduleExample__img")) {
+    let img = document.createElement("img");
+    img.classList.add("scheduleExample__img");
+    img.setAttribute("src", "example.png");
+    scheduleExample.append(img);
+  }
+
+  modalUnderlay.onclick = function() {
+    hideModal(scheduleExample);
+  }
+
+  window.onkeydown = function() {
+    hideModal(scheduleExample);
+  }
+}
+
+function showModal(el) {
+  document.documentElement.style.overflow = "hidden";
+  el.classList.add("modal--show");
+  modalUnderlay.classList.add("modalUnderlay--show");
+}
+
+function hideModal(el) {
+  document.documentElement.style.overflow = "auto";
+  el.classList.remove("modal--show");
+  modalUnderlay.classList.remove("modalUnderlay--show");
+
+  window.onkeydown = false;
+  modalUnderlay.onclick = false;
 }
